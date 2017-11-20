f589731855cd344eac58eec41b3cf778caeea410
wiltrahan <twilorip@gmail.com>
2017-11-20 12:38:28 -0500

    forLoops

---
+++ Iterations/Iterations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterations
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            for (int i = 10; i >= 1; i--)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}

8f6e0a1776d11e28267e4d327fcdabd3cbb3a8ff
wiltrahan <twilorip@gmail.com>
2017-11-20 15:57:50 -0500

    FileandFileInfo

---
+++ FileandFileInfo/FileandFileInfo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FileandFileInfo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"c:\somefile.jpg";
+            
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+            var content = File.ReadAllText(path);
+            
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
+            
+        }
+    }
+}

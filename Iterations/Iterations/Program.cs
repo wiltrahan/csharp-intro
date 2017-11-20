b9543dee84dc93be0e17b5855a7ba9c9ddec8eb8
wiltrahan <twilorip@gmail.com>
2017-11-20 12:42:06 -0500

    forEach

--- Iterations/Iterations/Program.cs
+++ Iterations/Iterations/Program.cs
@@ -7,6 +7,7 @@ namespace Iterations
     {
         public static void Main(string[] args)
         {
+            /*
             for (int i = 1; i <= 10; i++)
             {
                 if (i % 2 == 0)
@@ -21,6 +22,27 @@ namespace Iterations
                 {
                     Console.WriteLine(i);
                 }
+            }
+            */
+
+            var name = "John Smith";
+            /*
+            for (int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+           
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+            */
+
+            var numbers = new int[] {1, 2, 3, 4};
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
             }
         }
     }

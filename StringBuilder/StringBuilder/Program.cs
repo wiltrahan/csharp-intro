4885912de1faa1581c7ce28e5861884492425afe
wiltrahan <twilorip@gmail.com>
2017-11-20 15:34:51 -0500

    StringBuilder

---
+++ StringBuilder/StringBuilder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringBuilder
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new System.Text.StringBuilder("Hello World!");
+            
+            /*
+            builder.Append('-', 10);
+            builder.AppendLine();
+            builder.Append("Header");
+            builder.AppendLine();
+            builder.Append('-', 10);
+            */
+            //above is the same as chaining below
+            builder
+                .Append('-', 10)
+                .AppendLine()
+                .Append("Header")
+                .AppendLine()
+                .Append('-', 10);
+
+            builder.Replace('-', '+');
+
+            builder.Remove(0, 10);
+
+            builder.Insert(0, new string('-', 10));
+            
+            Console.WriteLine(builder);
+
+            Console.WriteLine("First char: " + builder[0]);
+        }
+    }
+}

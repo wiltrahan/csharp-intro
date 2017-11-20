5604d6d28dd9b684f38a92dd90d48f105b03fefe
wiltrahan <twilorip@gmail.com>
2017-11-20 14:50:02 -0500

    dateTime

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DateTime
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var dateTime = new System.DateTime(2017, 11, 20);
+            var now = System.DateTime.Now;
+            var today = System.DateTime.Today;
+
+            //Console.WriteLine("Hour: " + now.Hour);
+            //Console.WriteLine("Minute: " + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
+        }
+    }
+}

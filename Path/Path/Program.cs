b8a83a14131f02d386cbba38448c3d7a685adb43
wiltrahan <twilorip@gmail.com>
2017-11-20 16:12:56 -0500

    Path

---
+++ Path/Path/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Path
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var path = @"C:\Projects\CSharpFundamentals\HelloWorld\HelloWorld.sln";
+
+            var dotIndex = path.IndexOf('.');
+            var extension = path.Substring(dotIndex);
+
+            /*
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetExtension(path));
+            Console.WriteLine("File name w/out extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory name: " + Path.GetDirectoryName(path));
+            */
+        }
+    }
+}

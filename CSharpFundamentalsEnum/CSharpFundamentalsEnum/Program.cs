c2c1c7d43f050618f6e4dfd0e28f6cb3c4477b0d
wiltrahan <twilorip@gmail.com>
2017-11-20 11:38:06 -0500

    Enum

---
+++ CSharpFundamentalsEnum/CSharpFundamentalsEnum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpFundamentalsEnum
+{
+
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 1
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+
+            var methodId = 3;
+            Console.WriteLine((ShippingMethod)methodId);
+
+            Console.WriteLine(method.ToString());
+
+            var methodName = "Express";
+
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}

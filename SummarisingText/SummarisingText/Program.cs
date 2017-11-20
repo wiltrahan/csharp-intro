2fe6220d0124f9d9bb2c66504f01df28a9c0e18c
wiltrahan <twilorip@gmail.com>
2017-11-20 15:24:43 -0500

    SummarisingText

---
+++ SummarisingText/SummarisingText/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarisingText
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really really long text.";
+            var summary = StringUtility.SummerizeText(sentence, 25);
+            Console.WriteLine(summary);
+
+        }   
+    }
+}
---
+++ SummarisingText/SummarisingText/StringUtility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SummarisingText
+{
+    public class StringUtility
+    {
+        public static string SummerizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+                return text;
+           
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length + 1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+            return String.Join(" ", summaryWords) + "...";
+        }
+    }
+}

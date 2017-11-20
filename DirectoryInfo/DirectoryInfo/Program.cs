00e31fd83fb6e851c7b9ef38298a8f2dd58d9066
wiltrahan <twilorip@gmail.com>
2017-11-20 16:07:02 -0500

    directoryInfo

---
+++ DirectoryInfo/DirectoryInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+namespace DirectoryInfo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"c:\projects\CSharpFundamentals", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\projects\CSharpFundamentals", "*.*",
+                SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+            Directory.Exists("...");
+
+            var directoryInfo = new System.IO.DirectoryInfo("...");
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+        }
+    }
+}
